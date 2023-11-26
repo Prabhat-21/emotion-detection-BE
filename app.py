8d14a3f98efb123ca39ee017307b4e5267426125
Prabhat-21 <sharma.prabhat666@gmail.com>
2023-11-26 16:14:19 +0530

    requirement changes

--- app.py
+++ app.py
@@ -17,12 +17,8 @@ import redis
 import json
 import os
 
-
-haar_file=cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-face_cascade=cv2.CascadeClassifier(haar_file)
-
-
-
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
 
 server_email = "emotiondetectionserver@gmail.com"
 server_password = "iynztbhliitypkxn"
@@ -32,7 +28,6 @@ app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
 labels = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 json_file = open('models/emotion_model.json', 'r')
 loaded_model_json = json_file.read()
@@ -41,8 +36,6 @@ emotion_model = model_from_json(loaded_model_json)
 
 emotion_model.load_weights("models/emotion_model.h5")
 print("Loaded models from disk")
-
-face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
 connection = mysql.connector.connect(
     host="localhost",
@@ -195,25 +188,26 @@ def index():
 
 def extract_features(image):
     feature = np.array(image)
-    feature = feature.reshape(1,48,48,1)
-    return feature/255.0
+    feature = feature.reshape(1, 48, 48, 1)
+    return feature / 255.0
 
 
 def predict_emotion(path):
     frame = cv2.imread(path, cv2.IMREAD_COLOR)
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces=face_cascade.detectMultiScale(gray,1.3,5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     try:
-        for (p,q,r,s) in faces:
-            image = gray[q:q+s,p:p+r]
-            cv2.rectangle(frame,(p,q),(p+r,q+s),(255,0,0),2)
-            image = cv2.resize(image,(48,48))
+        for (p, q, r, s) in faces:
+            image = gray[q:q + s, p:p + r]
+            cv2.rectangle(frame, (p, q), (p + r, q + s), (255, 0, 0), 2)
+            image = cv2.resize(image, (48, 48))
             img = extract_features(image)
             pred = emotion_model.predict(img)
             prediction_label = labels[pred.argmax()]
             # print("Predicted Output:", prediction_label)
             # cv2.putText(im,prediction_label)
-            cv2.putText(frame, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,3, (0,0,255))
+            cv2.putText(frame, '% s' % (prediction_label), (p - 10, q - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 3,
+                        (0, 0, 255))
         # cv2.imshow("Output",frame)
         output_path = os.path.join('static/uploads', str(uuid1()) + ".png")
         cv2.imwrite(output_path, frame)
